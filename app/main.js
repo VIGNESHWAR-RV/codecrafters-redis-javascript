35cdc243063a6693c318a58fad6c2428fbb88320
Vigneshwar <vigneshwarrv666@gmail.com>
2026-04-07 00:38:47 +0530

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -4,6 +4,8 @@ const RespParser = require('respjs');
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
+const redisLookup = {};
+
 // Uncomment the code below to pass the first stage
 const server = net.createServer((connection) => {
   // Handle connection
@@ -19,6 +21,20 @@ const server = net.createServer((connection) => {
       case 'ECHO': {
         const [type , detailsToTalkBack] = req;
         const res = RespParser.encodeBulk(detailsToTalkBack).toString();
+        connection.write(res);
+        break;
+      }
+      case 'SET': {
+        const [type, key, value] = req;
+        redisLookup[key] = value;
+        const res = RespParser.encodeString('OK').toString();
+        connection.write(res);
+        break;
+      }
+      case 'GET': {
+        const [type, key] = req;
+        const value = redisLookup[key];
+        const res = RespParser.encodeBulk(value).toString();
         connection.write(res);
         break;
       }
