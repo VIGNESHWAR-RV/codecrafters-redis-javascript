f9d93a2cac44b485ab58fc2326ff2a186cbc74cb
Vigneshwar <vigneshwarrv666@gmail.com>
2026-04-24 20:00:20 +0530

    codecrafters submit [skip ci]

--- app/lib/commands/rpush.js
+++ app/lib/commands/rpush.js
@@ -1,14 +1,14 @@
 const { redisLookup } = require("../inMemoryLookup/index");
 const { encodeToRespInteger } = require("../respParser/index");
 
-function rPushCommand(listName, value) {
+function rPushCommand(listName, ...values) {
   let list = redisLookup[listName];
   if (!list) {
     redisLookup[listName] = [];
     list = redisLookup[listName];
   }
 
-  list.push(value);
+  list.push(...values);
   const res = encodeToRespInteger(list.length);
   return res;
 }
