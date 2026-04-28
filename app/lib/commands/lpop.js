426c774a00df503e18cfc241a96346fc4c61bc59
Vigneshwar <vigneshwarrv666@gmail.com>
2026-04-28 17:24:10 +0530

    codecrafters submit [skip ci]

--- app/lib/commands/lpop.js
+++ app/lib/commands/lpop.js
@@ -1,17 +1,39 @@
 const { logger } = require("../contextualLogger");
 const { redisLookup } = require("../inMemoryLookup/index");
-const { encodeToRespBulkString } = require("../respParser/index");
+const {
+  encodeToRespBulkString,
+  encodeToRespArray,
+} = require("../respParser/index");
 
-function lPopCommand(listName) {
+function lPopCommand(listName, elementsCount = 1) {
+  elementsCount = +elementsCount;
   const list = redisLookup[listName];
 
   if (!list) {
     return encodeToRespBulkString(null);
   }
 
-  const val = list.shift();
-  const res = encodeToRespBulkString(val);
+  if (elementsCount > list.length) {
+    elementsCount = list.length;
+  }
 
+  const removedValues = [];
+
+  for (let i = 0; i < elementsCount; i++) {
+    const removedValue = list.shift();
+    removedValues.push(removedValue);
+  }
+
+  if (!removedValues.length) {
+    return encodeToRespBulkString(null);
+  }
+
+  if (removedValues.length === 1) {
+    const res = encodeToRespBulkString(removedValues[0]);
+    return res;
+  }
+
+  const res = encodeToRespArray(removedValues);
   return res;
 }
 
