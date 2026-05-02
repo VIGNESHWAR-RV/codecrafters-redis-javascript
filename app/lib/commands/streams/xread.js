d94cffd316f9a43bf1bac925097dc07fd6b11334
Vigneshwar <vigneshwarrv666@gmail.com>
2026-05-02 19:43:25 +0530

    codecrafters submit [skip ci]

--- app/lib/commands/streams/xread.js
+++ app/lib/commands/streams/xread.js
@@ -19,9 +19,7 @@ function xReadCommand(type, stream_key, id) {
   });
 
   const res = [encodeToRespBulkString(stream_key), resRecords];
-
-  console.log(encodeToRespArray(res).toString());
-  return encodeToRespArray(res);
+  return encodeToRespArray([res]);
 }
 
 module.exports = {
