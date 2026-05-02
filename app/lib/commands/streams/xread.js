2489cb09b2000e59dd593cee99d794a6a696bb44
Vigneshwar <vigneshwarrv666@gmail.com>
2026-05-02 19:54:38 +0530

    codecrafters submit [skip ci]

--- app/lib/commands/streams/xread.js
+++ app/lib/commands/streams/xread.js
@@ -4,21 +4,38 @@ const {
   encodeToRespArray,
 } = require("../../respParser");
 
-function xReadCommand(type, stream_key, id) {
-  const { entries } = redisLookup?.[stream_key] ?? {};
+function xReadCommand(type, ...streamKeysAndIds) {
+  const res = [];
 
-  const [queryMilliSecondId, querySequenceId] = id?.split("-").map((el) => +el);
+  const stream_keys = [];
+  const ids = [];
 
-  const resRecords = [];
-  entries?.forEach(({ id, args }) => {
-    if (id[0] >= queryMilliSecondId && id[1] > querySequenceId) {
-      const encodedRecordId = encodeToRespBulkString(id.join("-"));
-      const encodedArgs = args.map(encodeToRespBulkString);
-      resRecords.push([encodedRecordId, encodedArgs]);
-    }
-  });
+  const streamKeysLength = streamKeysAndIds.length / 2;
+  for (let i = 0; i < streamKeysLength; i++) {
+    stream_keys.push(streamKeysAndIds[i]);
+    ids.push(streamKeysAndIds[i + streamKeysLength]);
+  }
 
-  const res = [encodeToRespBulkString(stream_key), resRecords];
+  for (let j = 0; j < stream_keys.length; j++) {
+    const stream_key = stream_keys[j];
+    const id = ids[j];
+
+    const { entries } = redisLookup[stream_key];
+    const [queryMilliSecondId, querySequenceId] = id
+      ?.split("-")
+      .map((el) => +el);
+
+    const resRecords = [];
+    entries?.forEach(({ id, args }) => {
+      if (id[0] >= queryMilliSecondId && id[1] > querySequenceId) {
+        const encodedRecordId = encodeToRespBulkString(id.join("-"));
+        const encodedArgs = args.map(encodeToRespBulkString);
+        resRecords.push([encodedRecordId, encodedArgs]);
+      }
+    });
+
+    res.push([encodeToRespBulkString(stream_key), resRecords]);
+  }
   return encodeToRespArray([res]);
 }
 
