8bfb54045a9ea0bd86bb4103ccea2a6c95987bda
Vigneshwar <vigneshwarrv666@gmail.com>
2026-05-05 13:34:47 +0530

    codecrafters submit [skip ci]

--- app/lib/commands/transactions/incr.js
+++ app/lib/commands/transactions/incr.js
@@ -1,15 +1,14 @@
 const { redisLookup } = require("../../inMemoryLookup");
 const { encodeToRespInteger } = require("../../respParser");
 
-
 function incrCommand(key) {
   const val = redisLookup[key];
 
-  val.value = +value.value + 1;
+  val.value = +val.value + 1;
 
   return encodeToRespInteger(val.value);
 }
 
 module.exports = {
-    incrCommand
-}
+  incrCommand,
+};
