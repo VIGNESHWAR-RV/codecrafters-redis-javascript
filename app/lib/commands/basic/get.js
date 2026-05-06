c435b443ff99a3f4ffdcd9c724f336dbc7a6fef1
Vigneshwar <vigneshwarrv666@gmail.com>
2026-05-06 15:58:26 +0530

    codecrafters submit [skip ci]

--- app/lib/commands/basic/get.js
+++ app/lib/commands/basic/get.js
@@ -5,12 +5,17 @@ const {
 const { redisLookup } = require("../../inMemoryLookup");
 
 function getCommand(key) {
-  let { value = null, expiryTimeStamp } = redisLookup?.[key] ?? {};
+  let { value, expiryTimeStamp } = redisLookup?.[key] ?? {};
   if (expiryTimeStamp && Date.now() >= expiryTimeStamp) {
     delete redisLookup[key];
     const res = encodeToRespNull();
     return res;
   }
+
+  if (!value) {
+    return encodeToRespNull();
+  }
+
   const res = encodeToRespBulkString(value);
   return res;
 }
