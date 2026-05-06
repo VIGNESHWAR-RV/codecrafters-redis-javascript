7d13fa136d3b3bceb4eda649a056fe2ed4498544
Vigneshwar <vigneshwarrv666@gmail.com>
2026-05-06 17:25:25 +0530

    codecrafters submit [skip ci]

---
+++ app/lib/commands/transactions/discard.js
@@ -0,0 +1,13 @@
+const { clientLookup } = require("../../inMemoryLookup");
+const { encodeToRespString } = require("../../respParser");
+
+function discardCommand(clientId) {
+  const clientData = clientLookup(clientId);
+
+  if (!clientData.queuedCommands) {
+    throw new Error("DISCARD without MULTI");
+  }
+
+  delete clientData.queuedCommands;
+  return encodeToRespString("OK");
+}
