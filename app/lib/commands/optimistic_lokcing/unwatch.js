05f2d561604360b425f71fa1d0a9f1ae96a25c60
Vigneshwar <vigneshwarrv666@gmail.com>
2026-05-07 20:04:30 +0530

    codecrafters submit [skip ci]

--- app/lib/commands/optimistic_lokcing/unwatch.js
+++ app/lib/commands/optimistic_lokcing/unwatch.js
@@ -1,4 +1,5 @@
 const { clientLookup } = require("../../inMemoryLookup");
+const { encodeToRespString } = require("../../respParser");
 
 function unWatchCommand(clientId) {
   const { watchedkeys } = clientLookup[clientId];
@@ -6,6 +7,8 @@ function unWatchCommand(clientId) {
   if (watchedkeys) {
     delete clientLookup[clientId].watchedkeys;
   }
+
+  return encodeToRespString("OK");
 }
 
 module.exports = {
