47e522bd972c6fff13fa392c6d365a2fd9d2b6d3
Vigneshwar <vigneshwarrv666@gmail.com>
2026-05-15 20:10:34 +0530

    codecrafters submit [skip ci]

--- app/lib/commands/index.js
+++ app/lib/commands/index.js
@@ -89,7 +89,7 @@ const COMMANDS_TO_BE_NOTIFIED_TO_REPLICA = {
 async function executeAvailableCommand(clientId, reqData) {
   const startTime = Date.now();
   try {
-    logger.debug(`raw request ->`, reqData);
+    logger.debug(`raw request ->`, reqData.toString());
     const [reqType, ...reqDetails] = decodeResp(reqData);
     logger.info(reqType);
     logger.debug(`request details ->`, reqDetails);
